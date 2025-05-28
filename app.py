db45cc1f444ae0cf06f6dbdf092080f8a48eee08
Uswa-cell <uswahmughal0@gmail.com>
2025-05-28 21:26:54 +0500

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os  
 
 app = Flask(__name__)
 
@@ -32,3 +33,8 @@ def tip():
         '''
     else:
         return '<p>Topic not found. Try "missing" or "encoding".</p>'
+
+# 🔻 This part is required for Render to work
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
